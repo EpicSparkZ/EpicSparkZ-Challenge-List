c5983f90db5cb6f85cc0e88b0b6fc74461c42b79
EpicSparkZ <epicsparkz@hotmail.com>
2025-05-10 11:49:21 -0600

    Update content.js

--- js/content.js
+++ js/content.js
@@ -83,7 +83,7 @@ export async function fetchEditors() {
 
 export async function fetchLeaderboard() {
     const list = await fetchList();
-
+    const packResult = await (await fetch(`${dir}/_packlist.json`)).json();
     const scoreMap = {};
     const errs = [];
     list.forEach(([level, err], rank) => {
