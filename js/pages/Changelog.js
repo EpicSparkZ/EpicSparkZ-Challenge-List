a51a8ef257201c008e365926b68a519a326578c7
EpicSparkZ <epicsparkz@hotmail.com>
2025-05-10 17:50:06 -0600

    Update Changelog.js

--- js/pages/Changelog.js
+++ js/pages/Changelog.js
@@ -1,33 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-  <meta charset="UTF-8" />
-  <title>Version Display</title>
-  <style>
-    body {
-      margin: 0;
-      padding: 0;
-      background-color: #111;
-      color: #eee;
-      font-family: Arial, sans-serif;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      height: 100vh;
-      flex-direction: column;
-    }
-
-    .version {
-      font-size: 72px;
-      font-weight: bold;
-    }
-
-    .subtitle {
-      font-size: 24px;
-      color: #ccc;
-    }
-  </style>
-</head>
+    
 <body>
   <div class="version">v1.0.0</div>
   <div class="subtitle">mrow meow</div>
