2089e5f012aaf3deb015bcbf73480ce1afef96b8
EpicSparkZ <epicsparkz@hotmail.com>
2025-05-10 17:28:45 -0600

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -16,7 +16,7 @@ const roleIconMap = {
 
 export default {
     components: { Spinner, LevelAuthors },
-    template: 
+    template: `
         <main v-if="loading">
             <Spinner></Spinner>
         </main>
@@ -33,16 +33,16 @@ export default {
                                 class="type-label-lg"
                                 :style="{ color: tributeColor }"
                             >Tribute</p>
-                            <p v-else-if="i === 0" class="type-label-lg" style="color: gold;">#1</p>
-                            <p v-else-if="i === 1" class="type-label-lg" style="color: silver;">#2</p>
-                            <p v-else-if="i === 2" class="type-label-lg" style="color: #cd7f32;">#3</p>
+                            <p v-else-if="i === 0" class="type-label-lg" :style="rankStyle(0)">#1</p>
+                            <p v-else-if="i === 1" class="type-label-lg" :style="rankStyle(1)">#2</p>
+                            <p v-else-if="i === 2" class="type-label-lg" :style="rankStyle(2)">#3</p>
                             <p v-else-if="i + 1 <= 31" class="type-label-lg">#{{ i + 1 }}</p>
                             <p v-else-if="i + 1 <= 51" class="type-label-lg">Legacy</p>
                             <p v-else class="type-label-lg">Super Legacy</p>
                         </td>
                         <td class="level" :class="{ 'active': selected == i, 'error': !level }">
                             <button @click="selected = i">
-                                <span class="type-label-lg">{{ level?.name || \Error (\${err}.json)\ }}</span>
+                                <span class="type-label-lg">{{ level?.name || \`Error (\${err}.json)\` }}</span>
                             </button>
                         </td>
                     </tr>
@@ -85,7 +85,7 @@ export default {
                                 <a :href="record.link" target="_blank" class="type-label-lg">{{ record.user }}</a>
                             </td>
                             <td class="mobile">
-                                <img v-if="record.mobile" :src="\/assets/phone-landscape\${store.dark ? '-dark' : ''}.svg\" alt="Mobile">
+                                <img v-if="record.mobile" :src="\`/assets/phone-landscape\${store.dark ? '-dark' : ''}.svg\`" alt="Mobile">
                             </td>
                             <td class="hz">
                                 <p>{{ record.hz }}Hz</p>
@@ -97,37 +97,8 @@ export default {
                     <p>Looks like something failed to load or is broken at the moment. This may be due to a failure in code, or an invalid level file. Please contact a list developer if this continues for over 24 hours.</p>
                 </div>
             </div>
-            <div class="meta-container">
-                <div class="meta">
-                    <div class="errors" v-show="errors.length > 0">
-                        <p class="error" v-for="error of errors">{{ error }}</p>
-                    </div>
-                    <div class="og">
-                        <p class="type-label-md">Website layout made by <a href="https://tsl.pages.dev/" target="_blank">TheShittyList</a></p>
-                    </div>
-                    <template v-if="editors">
-                        <h3>List Editors</h3>
-                        <ol class="editors">
-                            <li v-for="editor in editors">
-                                <img :src="\/assets/\${roleIconMap[editor.role]}\${store.dark ? '-dark' : ''}.svg\" :alt="editor.role">
-                                <a v-if="editor.link" class="type-label-lg link" target="_blank" :href="editor.link">{{ editor.name }}</a>
-                                <p v-else>{{ editor.name }}</p>
-                            </li>
-                        </ol>
-                    </template>
-                    <h3>Submission Requirements</h3>
-                    <p>Achieved the record without using hacks (however, FPS bypass is allowed, up to 360fps)</p>
-                    <p>Achieved the record on the level that is listed on the site - please check the level ID before you submit a record</p>
-                    <p>Have either source audio or clicks/taps in the video. Edited audio only does not count</p>
-                    <p>The recording must have a previous attempt and entire death animation shown before the completion, unless the completion is on the first attempt. Everyplay records are exempt from this</p>
-                    <p>The recording must also show the player hit the endwall, or the completion will be invalidated.</p>
-                    <p>Do not use secret routes or bug routes</p>
-                    <p>Do not use easy modes, only a record of the unmodified level qualifies</p>
-                    <p>Once a level falls onto the Legacy List, we accept records for it for 24 hours after it falls off, then afterwards we never accept records for said level</p>
-                </div>
-            </div>
         </main>
-    ,
+    `,
     data: () => ({
         list: [],
         editors: [],
@@ -138,7 +109,6 @@ export default {
         store,
         tributeColor: '#ff0000',
         tributeGlow: '0 0 15px rgba(255, 0, 0, 0.85)',
-        levelRows: [],
     }),
     computed: {
         level() {
@@ -161,7 +131,7 @@ export default {
             this.errors.push(
                 ...this.list
                     .filter(([_, err]) => err)
-                    .map(([_, err]) => Failed to load level. (${err}.json))
+                    .map(([_, err]) => `Failed to load level. (${err}.json)`)
             );
             if (!this.editors) {
                 this.errors.push("Failed to load list editors.");
@@ -190,11 +160,18 @@ export default {
             };
 
             setInterval(() => {
-                this.tributeColor = hsl(${hue}, 100%, 65%);
+                this.tributeColor = `hsl(${hue}, 100%, 65%)`;
                 const [r, g, b] = hslToRgb(hue, 100, 65);
-                this.tributeGlow = 0 0 15px rgba(${r}, ${g}, ${b}, 0.80);
+                this.tributeGlow = `0 0 15px rgba(${r}, ${g}, ${b}, 0.80)`;
                 hue = (hue + speed) % 360;
             }, interval);
-        }
+        },
+        rankStyle(rank) {
+            const colors = ['gold', 'silver', '#cd7f32'];
+            let color = colors[rank];
+            return {
+                color: color,
+            };
+        },
     },
 };
