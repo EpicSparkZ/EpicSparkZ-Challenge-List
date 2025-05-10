eb542aeaa02b550694a94bb43e091d578bdf4b9c
EpicSparkZ <epicsparkz@hotmail.com>
2025-05-09 20:59:12 -0600

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -16,7 +16,7 @@ const roleIconMap = {
 
 export default {
     components: { Spinner, LevelAuthors },
-    template: 
+    template: `
         <main v-if="loading">
             <Spinner></Spinner>
         </main>
@@ -24,24 +24,23 @@ export default {
             <div class="list-container">
                 <table class="list" v-if="list">
                     <tr v-for="([level, err], i) in list">
-<td class="rank">
-    <p v-if="level?.name === 'HAUNTED'" class="type-label-lg">RIP</p>
-    <p v-else-if="i + 1 <= 31" class="type-label-lg">#{{ i + 1 }}</p>
-    <p v-else-if="i + 1 <= 51" class="type-label-lg">Legacy</p>
-    <p v-else class="type-label-lg">Super Legacy</p>
-</td>
-
+                        <td class="rank">
+                            <p v-if="level?.name === 'HAUNTED'" class="type-label-lg rip-label">RIP</p>
+                            <p v-else-if="i + 1 <= 31" class="type-label-lg">#{{ i + 1 }}</p>
+                            <p v-else-if="i + 1 <= 51" class="type-label-lg">Legacy</p>
+                            <p v-else class="type-label-lg">Super Legacy</p>
+                        </td>
                         <td class="level" :class="{ 'active': selected == i, 'error': !level }">
                             <button @click="selected = i">
-                                <span class="type-label-lg">{{ level?.name || \Error (\${err}.json)\ }}</span>
+                                <span class="type-label-lg">{{ level?.name || \`Error (\${err}.json)\` }}</span>
                             </button>
                         </td>
                     </tr>
                 </table>
             </div>
-            <div class="level-container">
+            <div class="level-container" :class="{ 'rip-background': level?.name === 'HAUNTED' }">
                 <div class="level" v-if="level">
-                    <h1>{{ level.name }}</h1>
+                    <h1 :class="{ 'rip-title': level.name === 'HAUNTED' }">{{ level.name }}</h1>
                     <LevelAuthors :author="level.author" :creators="level.creators" :verifier="level.verifier"></LevelAuthors>
                     <iframe class="video" id="videoframe" :src="video" frameborder="0"></iframe>
                     <ul class="stats">
@@ -71,7 +70,7 @@ export default {
                                 <a :href="record.link" target="_blank" class="type-label-lg">{{ record.user }}</a>
                             </td>
                             <td class="mobile">
-                                <img v-if="record.mobile" :src="\/assets/phone-landscape\${store.dark ? '-dark' : ''}.svg\" alt="Mobile">
+                                <img v-if="record.mobile" :src="\`/assets/phone-landscape\${store.dark ? '-dark' : ''}.svg\`" alt="Mobile">
                             </td>
                             <td class="hz">
                                 <p>{{ record.hz }}Hz</p>
@@ -80,7 +79,7 @@ export default {
                     </table>
                 </div>
                 <div v-else class="level" style="height: 100%; justify-content: center; align-items: center;">
-                    <p>Looks like something failed to load or is broken at the moment. This may be due to a faliure in code, or an invalid level file. Please contact a list developet if this continues for over 24 hours.</p>
+                    <p>Looks like something failed to load or is broken at the moment. This may be due to a failure in code, or an invalid level file. Please contact a list developer if this continues for over 24 hours.</p>
                 </div>
             </div>
             <div class="meta-container">
@@ -89,47 +88,31 @@ export default {
                         <p class="error" v-for="error of errors">{{ error }}</p>
                     </div>
                     <div class="og">
-                          <p class="type-label-md">Website layout made by <a href="https://tsl.pages.dev/" target="_blank">TheShittyList</a></p>
+                        <p class="type-label-md">Website layout made by <a href="https://tsl.pages.dev/" target="_blank">TheShittyList</a></p>
                     </div>
                     <template v-if="editors">
                         <h3>List Editors</h3>
                         <ol class="editors">
                             <li v-for="editor in editors">
-                                <img :src="\/assets/\${roleIconMap[editor.role]}\${store.dark ? '-dark' : ''}.svg\" :alt="editor.role">
+                                <img :src="\`/assets/\${roleIconMap[editor.role]}\${store.dark ? '-dark' : ''}.svg\`" :alt="editor.role">
                                 <a v-if="editor.link" class="type-label-lg link" target="_blank" :href="editor.link">{{ editor.name }}</a>
                                 <p v-else>{{ editor.name }}</p>
                             </li>
                         </ol>
                     </template>
                     <h3>Submission Requirements</h3>
-                    <p>
-                        Achieved the record without using hacks (however, FPS bypass is allowed, up to 360fps)
-                    </p>
-                    <p>
-                        Achieved the record on the level that is listed on the site - please check the level ID before you submit a record
-                    </p>
-                    <p>
-                        Have either source audio or clicks/taps in the video. Edited audio only does not count
-                    </p>
-                    <p>
-                        The recording must have a previous attempt and entire death animation shown before the completion, unless the completion is on the first attempt. Everyplay records are exempt from this
-                    </p>
-                    <p>
-                        The recording must also show the player hit the endwall, or the completion will be invalidated.
-                    </p>
-                    <p>
-                        Do not use secret routes or bug routes
-                    </p>
-                    <p>
-                        Do not use easy modes, only a record of the unmodified level qualifies
-                    </p>
-                    <p>
-                        Once a level falls onto the Legacy List, we accept records for it for 24 hours after it falls off, then afterwards we never accept records for said level
-                    </p>
+                    <p>Achieved the record without using hacks (however, FPS bypass is allowed, up to 360fps)</p>
+                    <p>Achieved the record on the level that is listed on the site - please check the level ID before you submit a record</p>
+                    <p>Have either source audio or clicks/taps in the video. Edited audio only does not count</p>
+                    <p>The recording must have a previous attempt and entire death animation shown before the completion, unless the completion is on the first attempt. Everyplay records are exempt from this</p>
+                    <p>The recording must also show the player hit the endwall, or the completion will be invalidated.</p>
+                    <p>Do not use secret routes or bug routes</p>
+                    <p>Do not use easy modes, only a record of the unmodified level qualifies</p>
+                    <p>Once a level falls onto the Legacy List, we accept records for it for 24 hours after it falls off, then afterwards we never accept records for said level</p>
                 </div>
             </div>
         </main>
-    ,
+    `,
     data: () => ({
         list: [],
         editors: [],
@@ -147,20 +130,13 @@ export default {
             if (!this.level.showcase) {
                 return embed(this.level.verification);
             }
-
-            return embed(
-                this.toggledShowcase
-                    ? this.level.showcase
-                    : this.level.verification
-            );
+            return embed(this.toggledShowcase ? this.level.showcase : this.level.verification);
         },
     },
     async mounted() {
-        // Hide loading spinner
         this.list = await fetchList();
         this.editors = await fetchEditors();
 
-        // Error handling
         if (!this.list) {
             this.errors = [
                 "Failed to load list. Retry in a few minutes or notify list staff.",
@@ -169,9 +145,7 @@ export default {
             this.errors.push(
                 ...this.list
                     .filter(([_, err]) => err)
-                    .map(([_, err]) => {
-                        return Failed to load level. (${err}.json);
-                    })
+                    .map(([_, err]) => `Failed to load level. (${err}.json)`)
             );
             if (!this.editors) {
                 this.errors.push("Failed to load list editors.");
