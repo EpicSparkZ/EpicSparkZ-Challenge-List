e5c2ef82dcbca9c835afeae93d8cd5dee496557e
EpicSparkZ <epicsparkz@hotmail.com>
2025-05-10 17:58:47 -0600

    Update Changelog.js

--- js/pages/Changelog.js
+++ js/pages/Changelog.js
@@ -1,1 +1,11 @@
-
+export default {
+  name: 'Changelog',
+  mounted() {
+    // Redirect to the new changelog site
+    window.location.href = 'https://eclchangelog.pages.dev';
+  },
+  render() {
+    // Optional: Return null or a placeholder while redirecting
+    return null;
+  }
+};
